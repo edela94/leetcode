14e2d3cf176981873d64ecc9a73a48a97f064dc3
Esteban de la Torre <edelatorre@gmail.com>
2018-02-18 09:53:13 -0800

    Add C utility files

---
+++ utility/utility.h
@@ -0,0 +1,8 @@
+#ifndef _UTILITY_H
+#define _UTILITY_H
+
+#define MAX(a,b)    (a>b) ? (a):(b)
+#define MIN(a,b)    (a<b) ? (a):(b)
+#define RANGE(min,max,n) (min < n && n < max) ? (1):(0)
+
+#endif // _UTILITY_H
